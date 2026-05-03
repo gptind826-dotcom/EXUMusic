61b2e313544ee18cc5f84bd0feff5e1c91e9d871
gptind826-dotcom <gptind826@gmail.com>
2026-05-03 17:05:05 +0600

    add AloneMusic/utils/pastebin.py

---
+++ AloneMusic/utils/pastebin.py
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2021-2022 by EXUTeam@Github, < https://github.com/EXUTeam >.
+#
+# This file is part of < https://github.com/EXUTeam/EXUMusic > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/EXUTeam/EXUMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+
+import aiohttp
+
+BASE = "https://batbin.me/"
+
+
+async def post(url: str, *args, **kwargs):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, *args, **kwargs) as resp:
+            try:
+                data = await resp.json()
+            except Exception:
+                data = await resp.text()
+        return data
+
+
+async def AloneBin(text):
+    resp = await post(f"{BASE}api/v2/paste", data=text)
+    if not resp["success"]:
+        return
+    link = BASE + resp["message"]
+    return link
