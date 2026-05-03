d46d6ab79b726ab4aa2d033e2cee6db676916f61
gptind826-dotcom <gptind826@gmail.com>
2026-05-03 17:04:00 +0600

    add main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "web"))
+
+from flask_dashboard import app
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
