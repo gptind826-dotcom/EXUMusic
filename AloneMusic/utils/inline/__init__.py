6c988e424facebd42a3f352d669047ce1b661af4
gptind826-dotcom <gptind826@gmail.com>
2026-05-03 17:05:12 +0600

    add AloneMusic/utils/inline/__init__.py

---
+++ AloneMusic/utils/inline/__init__.py
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2021-2022 by EXUTeam@Github, < https://github.com/EXUTeam >.
+#
+# This file is part of < https://github.com/EXUTeam/EXUMusic > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/EXUTeam/EXUMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from .extras import *
+from .help import *
+from .play import *
+from .queue import *
+from .settings import *
+from .speed import *
+from .start import *
