3ffaeba9170445616c7e33505efae23c39f07d5d
gptind826-dotcom <gptind826@gmail.com>
2026-05-03 17:05:17 +0600

    add AloneMusic/utils/inline/speed.py

---
+++ AloneMusic/utils/inline/speed.py
@@ -0,0 +1,50 @@
+#
+# Copyright (C) 2021-2022 by EXUTeam@Github, < https://github.com/EXUTeam >.
+#
+# This file is part of < https://github.com/EXUTeam/EXUMusic > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/EXUTeam/EXUMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+def speed_markup(_, chat_id):
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text="🕒 0.5x",
+                    callback_data=f"SpeedUP {chat_id}|0.5",
+                ),
+                InlineKeyboardButton(
+                    text="🕓 0.75x",
+                    callback_data=f"SpeedUP {chat_id}|0.75",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text=_["P_B_4"],
+                    callback_data=f"SpeedUP {chat_id}|1.0",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="🕤 1.5x",
+                    callback_data=f"SpeedUP {chat_id}|1.5",
+                ),
+                InlineKeyboardButton(
+                    text="🕛 2.0x",
+                    callback_data=f"SpeedUP {chat_id}|2.0",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text=_["CLOSE_BUTTON"],
+                    callback_data="close",
+                ),
+            ],
+        ]
+    )
+    return upl
