7f6a246f0c0f09b8b4bcc6a2dd77df8ba2f04247
gptind826-dotcom <gptind826@gmail.com>
2026-05-03 17:05:18 +0600

    add AloneMusic/utils/inline/start.py

---
+++ AloneMusic/utils/inline/start.py
@@ -0,0 +1,45 @@
+#
+# Copyright (C) 2021-2022 by EXUTeam@Github, < https://github.com/EXUTeam >.
+#
+# This file is part of < https://github.com/EXUTeam/EXUMusic > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/EXUTeam/EXUMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from pyrogram.types import InlineKeyboardButton
+
+import config
+from AloneMusic import app
+
+
+def start_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+            ),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+        ],
+    ]
+    return buttons
+
+
+def private_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+        [
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+        ],
+    ]
+    return buttons
